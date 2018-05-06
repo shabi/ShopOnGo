42405fb2b38543b8cefe318a615bc3b40d84a6a7
shabi <naqvi.shabi@gmail.com>
2018-05-06 13:00:37 +0530

    ShopOnGo: code setup

---
+++ ShopOnGo/ShopOnGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShopOnGo
+//
+//  Created by eCOM-shabi.naqvi on 06/05/18.
+//  Copyright © 2018 pixelgeniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
